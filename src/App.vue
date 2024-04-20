a128f1b9dfd9f9e9caff949675362b17a9670f27
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-04-20 11:38:41 -0300

    feat: adds css-doodle

--- src/App.vue
+++ src/App.vue
@@ -115,6 +115,35 @@ const copy = async () => {
       </section>
     </main>
   </div>
+
+  <css-doodle>
+    :doodle {
+    @grid: 1x3 / 100vmax;
+    position: absolute;
+    top: 0; left: 0;
+    z-index: 0;
+    }
+
+    @size: 100% 150%;
+    position: absolute;
+
+    background: @m(100, (
+    linear-gradient(transparent, @p(
+    #f9f871@repeat(2, @p([0-9a-f])),
+    #ff7fad@repeat(2, @p([0-9a-f]))
+    ))
+    @r(0%, 100%) @r(0%, 100%) /
+    @r(1px) @r(23vmin)
+    no-repeat
+    ));
+
+    will-change: transform;
+    animation: f 50s linear calc(-50s / @size() * @i()) infinite;
+    @keyframes f {
+    from { transform: translateY(-100%) }
+    to { transform: translateY(100%) }
+    }
+  </css-doodle>
 </template>
 
 <style scoped></style>
